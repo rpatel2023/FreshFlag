9131eaa105db6063172337b33340c87842e9127b
Raj Patel <151667813+rpatel2023@users.noreply.github.com>
2026-08-14 10:13:57 -0400

    test: update package import for FreshFlag rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stayfresh/models/grocery_item.dart';
+import 'package:freshflag/models/grocery_item.dart';
 
 void main() {
   group('GroceryItem persistence', () {
